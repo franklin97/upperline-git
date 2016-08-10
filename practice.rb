b6f80968a71c4e68d3c07adb8c351e98880741fe
franklin97 <franklinsalazar@mybihs.org>
2016-08-10 17:14:23 +0000

    new code

--- practice.rb
+++ practice.rb
@@ -1,5 +1,7 @@
-def crazy_strings (string1,string2)
-   puts string1.upcase.reverse
-  puts string2.gsub("s","z").reverse
-end
-crazy_strings("hello","friends")
+# def crazy_strings (string1,string2)
+#    puts string1.upcase.reverse
+#   puts string2.gsub("s","z").reverse
+# end
+# crazy_strings("hello","friends")
+# def speak_to_grandma
+farmer_tasks = ["Milk the cow", "Churn the butter", "Drive the tractor", "Peel potatoes", "Plant the corn"]
